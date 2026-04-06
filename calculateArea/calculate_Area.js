49824b458290101a6d72dd15ec8c3828c8bd33b8
JuanseCOURSERA <juandavid78520@gmail.com>
2026-04-06 04:49:34 -0400

    added to task

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,10 +1,25 @@
-let length;
-let width;
+// let length;
+// let width;
 
-function calculateArea(){
-    length = parseFloat(document.getElementById('length').value);
-    width = parseFloat(document.getElementById('width').value);
+// function calculateArea(){
+//     length = parseFloat(document.getElementById('length').value);
+//     width = parseFloat(document.getElementById('width').value);
 
-    let area = length * width;
-    document.getElementById('result').innerHTML = `El area del rectangulo es: ${area}`;
+//     let area = length * width;
+//     document.getElementById('result').innerHTML = `El area del rectangulo es: ${area}`;
+// }
+
+//TASK
+
+function recolectarEntradas(){
+    let grocery1 = parseFloat(document.getElementById('grocery1').value);
+    let grocery2 = parseFloat(document.getElementById('grocery2').value);
+    let grocery3 = parseFloat(document.getElementById('grocery3').value);
+
+    groceryTracker(grocery1, grocery2, grocery3);
+}
+
+function groceryTracker(grocery1, grocery2, grocery3){
+    let total = grocery1 + grocery2 + grocery3;
+    document.getElementById('result').innerHTML = `The total amount is: $${total}`;
 }
